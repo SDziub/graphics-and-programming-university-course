06ca6f3ec313d5fb4d02ba76c337f150eedebae4
EvilRadzio <Radzio89k.yt@wp.pl>
2026-05-19 22:31:45 +0200

    Add & opitmize all entity dragging Add entity spawn integrate world load/save

--- src/Game/Scenes/LevelEditor.cpp
+++ src/Game/Scenes/LevelEditor.cpp
@@ -5,26 +5,28 @@
 #include "Game/Map.hpp"
 #include <spdlog/spdlog.h>
 
-/*
-	poruszanie graczem myszka
-	
-*/
-
 Scenes::LevelEditor::LevelEditor(px::SceneInitCtx ctx, Context& gctx) :
 	Scene(ctx),
 	m_ctx(gctx),
-	LE_map(sf::Vector2u(mapWidth, mapHeight), m_ctx.tiles["empty"]) 
+	LE_world(),
+	LE_registry(LE_world.entities),
+	LE_map(LE_world.map)
 {
-	
 	for (const auto& [tilename, _ ] : m_ctx.tiles) {
 		TileName.push_back(tilename);
 	}
 
-	//for (const auto& [entityName, _] : m_ctx.entities) { }
+	for (const auto& [entityName, _] : m_ctx.entities.data()) {
+		entities.push_back(entityName);
+	}
 
-	//spawning entity
+	if (!m_ctx.maps.empty() && currentMap < m_ctx.maps.size()) {
+		mapPath = m_ctx.maps[currentMap];
+	}
+
 	player = m_ctx.entities.get("player").spawn(LE_registry);
 	auto pp = LE_registry.get<Transform>(player);
+	LE_registry.emplace<EntityType>(player, EntityType{ "player" });
 	SPDLOG_INFO("Spawned player ar {},{}", pp.pos.x, pp.pos.y);
 	LE_tiles = m_ctx.tiles;
 
@@ -46,43 +48,47 @@ void Scenes::LevelEditor::update(px::UpdateCtx& ctx)
 		static int prevWidth = mapWidth;
 		static int prevHeight = mapHeight;
 
-		ImGui::DragInt("##mapheight", &mapHeight, 0.5f,25,100); //Set map Height
-		if (ImGui::IsItemDeactivatedAfterEdit) {
+		ImGui::DragInt("##mapheight", &mapHeight, 0.5f,25,100);
+		if (ImGui::IsItemDeactivatedAfterEdit()) {
 			resizeMap();
 		}
-		ImGui::DragInt("##mapWidth", &mapWidth, 0.5f, 25, 100); //Set map Width
-		if (ImGui::IsItemDeactivatedAfterEdit) {
+		ImGui::DragInt("##mapWidth", &mapWidth, 0.5f, 25, 100);
+		if (ImGui::IsItemDeactivatedAfterEdit()) {
 			resizeMap();
 		}
 		
 		
 		static bool item_highlight = false;
 		int item_highlighted_idx = -1;
-		if (ImGui::BeginListBox("Choose tile")) //Choose tile list
+		if (ImGui::BeginListBox("Choose tile")) 
 		{
 			for (int n = 0; n < TileName.size(); n++)
 			{
 				const bool is_selected = (currentTile == n);
-				if (ImGui::Selectable(TileName[n].c_str(), is_selected))
+				if (ImGui::Selectable(TileName[n].c_str(), is_selected)) {
 					currentTile = n;
+					selectState = selected::tile;
+				}
 				if (item_highlight && ImGui::IsItemHovered())
 					item_highlighted_idx = n;
 
-				if (is_selected)
+				if (is_selected) {
 					ImGui::SetItemDefaultFocus();
+					selectState = 1;
+				}
 			}
 			
 			ImGui::EndListBox();
 			
-			if (ImGui::BeginListBox("Choose map")) //Choose map list
+			if (ImGui::BeginListBox("Choose map")) 
 			{
-
 				for (int n = 0; n < m_ctx.maps.size(); n++)
 				{
 					const bool is_selected = (currentMap == n);
-					if (ImGui::Selectable(m_ctx.maps[n].c_str(), is_selected))
+					if (ImGui::Selectable(m_ctx.maps[n].c_str(), is_selected)) {
 						currentMap = n;
 						mapPath = m_ctx.maps[currentMap];
+					}
 					if (item_highlight && ImGui::IsItemHovered())
 						item_highlighted_idx = n;
 
@@ -92,27 +98,37 @@ void Scenes::LevelEditor::update(px::UpdateCtx& ctx)
 				ImGui::EndListBox();
 			}
 
-			
-
-			//Input Map Name
 			static char buf5[255] = ""; ImGui::InputText("no blank", buf5, sizeof(buf5), ImGuiInputTextFlags_CharsNoBlank);
-			
-				if (ImGui::Button("New")) {
-					newMapName = buf5;
-					mapPathS = "./resources/maps/" + newMapName + ".json";
-					saveMap(mapPathS, LE_map);
-					m_ctx.maps.push_back(mapPathS);
-				}ImGui::SameLine();
 
-				if (ImGui::Button("Save")) {
-					saveMap(mapPath, LE_map);
-				}ImGui::SameLine();
-
-				if (ImGui::Button("Load")) {
-					LE_map = loadMap(mapPath, LE_tiles);
-					SPDLOG_INFO("Map size after load: {}x{}", LE_map.size().x, LE_map.size().y);
-					SPDLOG_INFO("Tile [0,0]: {}", LE_map.at({ 0,0 }).tileName);
+			if (ImGui::BeginListBox("Choose entity")) {
+				for (int e = 0; e < (int)entities.size(); e++) {
+					const bool is_selected = (currentEntity == e);
+					if (ImGui::Selectable(entities[e].c_str(), is_selected)) {
+						currentEntity = e;
+						selectState = selected::entity;
+					}
+					if (is_selected)
+						ImGui::SetItemDefaultFocus();
 				}
+				ImGui::EndListBox();
+			}
+			
+			if (ImGui::Button("New")) {
+				newMapName = buf5;
+				mapPathS = "./resources/maps/" + newMapName + ".json";
+				saveWorld(mapPathS, LE_world);
+				m_ctx.maps.push_back(mapPathS);
+			}ImGui::SameLine();
+
+			if (ImGui::Button("Save")) {
+				saveWorld(mapPath, LE_world);
+			}ImGui::SameLine();
+
+			if (ImGui::Button("Load")) {
+				loadWorld(mapPath, LE_tiles, LE_world);
+				SPDLOG_INFO("Map size after load: {}x{}", LE_world.map.size().x, LE_world.map.size().y);
+				SPDLOG_INFO("Tile [0,0]: {}", LE_world.map.at({ 0,0 }).tileName);
+			}
 		}
 		ImGui::End();
 	}
@@ -134,31 +150,48 @@ void Scenes::LevelEditor::update(px::UpdateCtx& ctx)
 	playerHitbox.position += playerTransform.pos;
 	playerRect = playerHitbox;
 
-	bool isDraggingPlayer = playerHitbox.contains(worldMousePos);
-	
-
+	static entt::entity draggedEntity = entt::null;
 	if (!ImGui::GetIO().WantCaptureMouse)
 	{
 		sf::Vector2i tileCoord = mousePosition / tileSize;
-
 		bool inBounds = tileCoord.x >= 0 && tileCoord.y >= 0
 			&& static_cast<unsigned>(tileCoord.x) < mapWidth
 			&& static_cast<unsigned>(tileCoord.y) < mapHeight;
 
-		if (api.mapping.isHeld(px::InputId::MLeft) && inBounds && !isDraggingPlayer) {
-				sf::Vector2u MP = static_cast<sf::Vector2u>(tileCoord);
-				LE_map.at(MP) = m_ctx.tiles.at(TileName[currentTile]);
-				
+		if (api.mapping.isPressed(px::InputId::MLeft)) {
+			draggedEntity = entt::null;
+			LE_registry.view<Transform, Hitbox>().each(
+				[&](entt::entity e, const Transform& t, const Hitbox& h) {
+					sf::FloatRect rect = h.rect;
+					rect.position += t.pos;
+					if (rect.contains(worldMousePos))
+						draggedEntity = e;
+				}
+			);
 		}
-		if (api.mapping.isHeld(px::InputId::MLeft) && inBounds && isDraggingPlayer) {
-			sf::Vector2i offset = api.mapping.getMouseDelta();
-			auto& transform = LE_registry.get<Transform>(player);
-			transform.pos += sf::Vector2f(offset) / float(tileSize);
+
+		if (api.mapping.isReleased(px::InputId::MLeft)) {
+			if (draggedEntity == entt::null && inBounds && selectState == selected::entity) {
+				if (currentEntity >= 0 && currentEntity < (int)entities.size()) {
+					auto e = m_ctx.entities.get(entities[currentEntity]).spawn(LE_registry);
+					LE_registry.emplace<EntityType>(e, EntityType{ entities[currentEntity] });
+					LE_registry.get<Transform>(e).pos = worldMousePos;
+				}
+			}
+			draggedEntity = entt::null;
 		}
-			
+
+		if (api.mapping.isHeld(px::InputId::MLeft)) {
+			if (draggedEntity != entt::null) {
+				auto& t = LE_registry.get<Transform>(draggedEntity);
+				t.pos += sf::Vector2f(api.mapping.getMouseDelta()) / float(tileSize);
+			}
+			else if (inBounds && selectState == selected::tile) {
+				LE_map.at(static_cast<sf::Vector2u>(tileCoord)) = m_ctx.tiles.at(TileName[currentTile]);
+			}
+		}
 	}
 	
-
 	if (api.mapping.isHeld(px::InputId::MRight)) {
 		sf::Vector2i mouseDiff = lastMousePos - currMousePos;
 		viewPosition += mouseDiff;
@@ -170,9 +203,9 @@ void Scenes::LevelEditor::draw(px::DrawCtx& ctx) const
 {	
 
 	sf::View view(sf::FloatRect{ (sf::Vector2f)viewPosition, (sf::Vector2f)ctx.window.getSize() });
-	uint32_t tileSide = windowSize / LE_map.size().x;
+	uint32_t tileSide = windowSize / LE_world.map.size().x;
 	ctx.window.setView(view);
-	for (size_t y = 0; y < LE_map.size().y; ++y) for (size_t x = 0; x < LE_map.size().x; ++x)
+	for (size_t y = 0; y < LE_world.map.size().y; ++y) for (size_t x = 0; x < LE_world.map.size().x; ++x)
 	{
 		sf::Vector2u position(x, y);
 		if (LE_map.at(position).sprite != "")
@@ -189,28 +222,33 @@ void Scenes::LevelEditor::draw(px::DrawCtx& ctx) const
 				static_cast<float>(tileSide) / bounds.size.x,
 				static_cast<float>(tileSide) / bounds.size.y
 			});
-
+			
 			ctx.window.draw(sprite);
 		}
 	}
-	sf::RectangleShape playerShape;
-	playerShape.setSize(playerRect.size * float(tileSide));
-	playerShape.setPosition(playerRect.position * float(tileSide));
-	playerShape.setFillColor(sf::Color::Blue);
-	ctx.window.draw(playerShape);
+
+	auto LEview = LE_registry.view<const px::Animation, const Transform>();
+
+	LEview.each([&](const auto& animation, const auto& transform) {
+		sf::Vector2f position = transform.pos * static_cast<float>(tileSide);
+
+		sf::Sprite sprite = animation.getSprite().value();
+		sprite.setPosition(position);
+		ctx.window.draw(sprite);
+		});
 
 }
 
 void Scenes::LevelEditor::resizeMap() {
-	auto oldMap = LE_map;
+	auto oldMap = LE_world.map;
 
-	LE_map = px::Grid<Tile>(sf::Vector2u(mapWidth, mapHeight), m_ctx.tiles.at("empty"));
+	LE_world.map = px::Grid<Tile>(sf::Vector2u(mapWidth, mapHeight), m_ctx.tiles.at("empty"));
 
-	for (unsigned int y = 0; y < std::min(oldMap.size().y, LE_map.size().y); y++)
+	for (unsigned int y = 0; y < std::min(oldMap.size().y, LE_world.map.size().y); y++)
 	{
-		for (unsigned int x = 0; x < std::min(oldMap.size().x, LE_map.size().x); x++)
+		for (unsigned int x = 0; x < std::min(oldMap.size().x, LE_world.map.size().x); x++)
 		{
-			LE_map.at({ x, y }) = oldMap.at({ x, y });
+			LE_world.map.at({ x, y }) = oldMap.at({ x, y });
 		}
 	}
 }
--- src/Game/Scenes/LevelEditor.hpp
+++ src/Game/Scenes/LevelEditor.hpp
@@ -4,6 +4,7 @@
 
 #include "Game/Context.hpp"
 #include "Game/Tile.hpp"
+#include "Game/World.hpp"
 #include <filesystem>
 
 namespace Scenes
@@ -20,29 +21,50 @@ namespace Scenes
 
 		
 	private:
-		entt::registry LE_registry;
-		entt::entity player;
-		std::vector<entt::entity> entities;
-		Context& m_ctx;
-		sf::Vector2i lastMousePos;
-		sf::Vector2i viewPosition{0,0};
-		std::unordered_map<std::string, Tile> LE_tiles;
+		enum selected
+		{
+			entity = 0,
+			tile = 1
+		};
+
+		//window / map config
 		int windowSize = 720;
 		int mapWidth = 25;
 		int mapHeight = 25;
 
-		px::Grid<Tile> LE_map;
+		World LE_world;
+		Context& m_ctx;
+		px::Grid<Tile>& LE_map;
+		entt::registry& LE_registry;
+		entt::entity player;
+		std::vector<std::string> entities;
+		
+		
+		
+		sf::Vector2i lastMousePos;
+		sf::Vector2i viewPosition{0,0};
+		
+		//map layout
+		std::unordered_map<std::string, Tile> LE_tiles;
+		std::unordered_map<std::string, std::pair<int, int>> LE_Entities;
+				
+		int selectState = selected::tile;
+		//selectable
 		int currentTile = 0;
 		int currentMap = 1;
-
+		int currentEntity = 1;
 		
-
-
+		//player & entity
 		sf::Rect<float>playerRect;
-		
+		bool isDragging = false;
+		sf::Vector2i dragStartPos;
+		const int dragThreshold = 4;
+
+		//tileSaving
 		std::vector<std::string> TileName;
 		std::vector<const char*> TileNameC;
-
+		
+		//saveLoad stuff
 		std::string newMapName;
 		std::string currMapName = std::to_string(currentMap);
 		std::string mapPath = "./resources/maps/" + currMapName + ".json";
